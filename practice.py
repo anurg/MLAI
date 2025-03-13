2e484e25f1b42b57ed5e96878a00f691c2e80b8e
Anurag <anurg@yahoo.com>
2025-03-13 15:20:52 +0530

    Openai gym practice

--- practice.py
+++ practice.py
@@ -1,6 +1,24 @@
-from agents import Agent, Runner
+from agent import Agent, Runner
+import asyncio
 
-agent = Agent(name="Assistant", instructions="You are very funny Assistant")
+spanish_agent=Agent(
+    name="spanish_agent",
+    instructions="You only speak in spanish"
+)
+english_agent=Agent(
+    name="english_agent",
+    instructions="You only speak in english."
+)
+triage_agent=Agent(
+    name="triage_agent",
+    instructions="Hand off the control to appropriate agent based on the language of request",
+    handoffs=[spanish_agent,english_agent]
+)
 
-result = Runner.run_sync(agent, "Write a funny joke about OpenAI and Sam Altman")
-print(result.final_output)
+async def main():
+    # result=await Runner.run(triage_agent,input="Hola, ¿cómo estás?")
+    result = await Runner.run(triage_agent,input="What is the capital of India")
+    print(result.final_output)
+
+if __name__ == "__main__":
+    asyncio.run(main())
