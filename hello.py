870543950693a67b45caf7e0888ded2690e8c88f
Anurag <anurg@yahoo.com>
2025-04-04 17:53:26 +0530

    Practice

--- hello.py
+++ hello.py
@@ -1,58 +1,38 @@
-from typing_extensions import TypedDict     
+from pprint import pprint
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import MessagesState
+from langchain_core.messages import HumanMessage,AIMessage, SystemMessage, AnyMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import START,END,StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
 
-class State(TypedDict):
-    graph_state: str
+def multiply(a:int, b:int) -> int:
+    """ mutiply two integers
+        a: first integer
+        b: second integer
+    """
+    return a * b
+tools = [multiply]
+llm = ChatOpenAI(model="gpt-4o")
+llm_with_tools=llm.bind_tools(tools)
+def tool_calling_llm(state:MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
-def node_1(state):
-    # print("---Node 1---")
-    return {"graph_state": state['graph_state'] +" I am"}
 
-def node_2(state):
-    # print("---Node 2---")
-    return {"graph_state": state['graph_state'] +" happy!"}
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm",tool_calling_llm)
+builder.add_node("tools",ToolNode(tools))
 
-def node_3(state):
-    # print("---Node 3---")
-    return {"graph_state": state['graph_state'] +" sad!"}
+builder.add_edge(START,"tool_calling_llm")
+builder.add_conditional_edges("tool_calling_llm",tools_condition)
+builder.add_edge("tools",END)
 
-import random
-from typing import Literal
-
-def decide_mood(state) -> Literal["node_2", "node_3"]:
-    
-    # Often, we will use state to decide on the next node to visit
-    user_input = state['graph_state'] 
-    
-    # Here, let's just do a 50 / 50 split between nodes 2, 3
-    if random.random() < 0.5:
-
-        # 50% of the time, we return Node 2
-        return "node_2"
-    
-    # 50% of the time, we return Node 3
-    return "node_3"
-
-from IPython.display import Image, display
-from langgraph.graph import StateGraph, START, END
-
-# Build graph
-builder = StateGraph(State)
-builder.add_node("node_1", node_1)
-builder.add_node("node_2", node_2)
-builder.add_node("node_3", node_3)
-
-# Logic
-builder.add_edge(START, "node_1")
-builder.add_conditional_edges("node_1", decide_mood)
-builder.add_edge("node_2", END)
-builder.add_edge("node_3", END)
-
-# Add
 graph = builder.compile()
 
-# View
-# display(Image(graph.get_graph().draw_mermaid_png()))
+response = graph.invoke({"messages" : HumanMessage(content="Multiply 2 and 3")})
 
-result = graph.invoke({"graph_state" : "Hi, this is Lance."})
+for m in response["messages"]:
+    m.pretty_print()
 
-print("Final state: ", result['graph_state'])
+
